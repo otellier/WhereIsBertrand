404a5f443be3d440f9f91be477673d840eb27178
otellier <olivier.tellier@etu.univ-lyon1.fr>
2018-04-30 13:20:41 +0200

    Add BaseActivity

--- WhereIsBertrand/app/src/main/java/com/example/iem/whereisbertrand/view/MainActivity.java
+++ WhereIsBertrand/app/src/main/java/com/example/iem/whereisbertrand/view/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.WindowManager;
 
 import com.example.iem.whereisbertrand.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //showFragment(HomeFragment.newInstance());
     }
 }
