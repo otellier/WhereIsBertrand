c3ec543caf3d1cb09d82c44ad666813f6992e56e
Thomas <thomas.chaboud@etu.univ-lyon1.fr>
2018-04-30 17:28:38 +0200

    OLIVIER LA BATTERIE

--- WhereIsBertrand/app/src/main/java/com/example/iem/whereisbertrand/manager/ManagerMicroController.java
+++ WhereIsBertrand/app/src/main/java/com/example/iem/whereisbertrand/manager/ManagerMicroController.java
@@ -1,5 +1,10 @@
 package com.example.iem.whereisbertrand.manager;
 
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.util.Log;
+import android.widget.ImageView;
+
 /**
  * Created by iem on 30/04/2018.
  */
@@ -12,9 +17,29 @@ public class ManagerMicroController {
     }
 
     private ManagerMicroController() {
+
     }
 
     public boolean isDeviceInRange(){
         return true;
     }
+
+    public void displayAnimation(final ImageView imageView, String fileName, int numberOfImages, Context context){
+
+        int signalStrength = 200;
+
+        final AnimationDrawable animation = new AnimationDrawable();
+        animation.setOneShot(false);
+
+        for(int i = 1; i <= numberOfImages; i++){
+            animation.addFrame(context.getResources().getDrawable(context.getResources().getIdentifier(fileName + "_" + i, "raw", context.getPackageName())), signalStrength);
+        }
+
+        imageView.setImageDrawable(animation);
+    }
+
+    public void makeABIP(){
+        Log.d("BIP", "Je suis un ane du BIP");
+    }
+
 }
--- WhereIsBertrand/app/src/main/java/com/example/iem/whereisbertrand/view/fragment/BluetoothResearchFragment.java
+++ WhereIsBertrand/app/src/main/java/com/example/iem/whereisbertrand/view/fragment/BluetoothResearchFragment.java
@@ -4,15 +4,21 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.iem.whereisbertrand.R;
+import com.example.iem.whereisbertrand.manager.ManagerMicroController;
 
 public class BluetoothResearchFragment extends BaseFragment {
     private View v;
     private Context context;
+    private ImageView imageView;
+    private Button buttonBIP;
 
     public static BluetoothResearchFragment newInstance() {
         
@@ -22,12 +28,22 @@ public class BluetoothResearchFragment extends BaseFragment {
         fragment.setArguments(args);
         return fragment;
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        v = inflater.inflate(R.layout.fragment_home, container, false);
+        v = inflater.inflate(R.layout.fragment_bluetooth_research, container, false);
         context = getActivity().getApplicationContext();
-        
+
+        imageView = v.findViewById(R.id.imageWave);
+        ManagerMicroController.getInstance().displayAnimation(imageView, "frame", 18, context);
+        buttonBIP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("CHEVRE", "La chèvre malicieuse est de retour, comme dans tout code android qui se respecte");
+                ManagerMicroController.getInstance().makeABIP();
+            }
+        });
         
         return v;
     }
